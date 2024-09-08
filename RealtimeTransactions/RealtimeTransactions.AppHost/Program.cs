fa85b7997de0fb1525e014f59f7478d4b5ccabea
Yannis TOCREAU <Yannis.TOCREAU@consulting-for.edenred.com>
2024-09-08 19:09:00 +0200

    feat: added all aspire dependencies

--- RealtimeTransactions/RealtimeTransactions.AppHost/Program.cs
+++ RealtimeTransactions/RealtimeTransactions.AppHost/Program.cs
@@ -3,13 +3,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 var kafka = builder.AddKafka("kafka")
     .WithKafkaUI();
 
-var rmq = builder.AddRabbitMQ("rabbitmq")
-    .WithRabbitMQUI();
+var rmq = builder.AddRabbitMQ("rabbitmq");
+var postgresql = builder.AddPostgres("postgres");
+var elastic = builder.AddElasticsearch("elastic");
+var seq = builder.AddSeq("seq");
 
-builder.AddProject<Projects.TransactionService>("transactionservice")
-    .WithReference(kafka);
+builder.AddProject<Projects.TransactionService>("transactions-api")
+    .WithReference(kafka)
+    .WithReference(postgresql)
+    .WithReference(elastic)
+    .WithReference(seq);
 
-builder.AddProject<Projects.NotificationService>("notificationservice")
-    .WithReference(kafka);
+builder.AddProject<Projects.NotificationService>("notifications-api")
+    .WithReference(kafka)
+    .WithReference(rmq)
+    .WithReference(seq);
 
 builder.Build().Run();
